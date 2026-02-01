da0b4994ee19bbf84d09c275acf0f53a66119955
zaighamrai25 <zaighamrai25@gmail.com>
2026-02-01 16:54:14 +0500

    Add Student class with properties and methods

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private double marks;
+
+    // Constructor
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    // Optional setter
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
